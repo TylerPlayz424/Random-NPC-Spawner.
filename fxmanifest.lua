f625dd5f95fbb80dab8aefe2338a52cbd998e377
TylerPlayz424 <95728044+TylerPlayz424@users.noreply.github.com>
2023-08-11 15:28:19 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'TylerPlayz424'
+description 'Random NPC spawner for FiveM'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'client.lua',
+}
